3ebaf2570e303f519e2fd1c496b2b9492a254968
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-06-30 23:51:49 +0530

    Add files via upload

---
+++ Security_Camera_2.py
@@ -0,0 +1,11 @@
+import cv2
+cam=cv2.VideoCapture(0)
+while cam.isOpened():
+    ret,frame1=cam.read()
+    ret,frame2=cam.read()
+    diff=cv2.absdiff(frame1,frame2)
+    if cv2.waitKey(10)==ord('q'):
+        break
+    cv2.imshow('Security Camera',diff)
+cam.release()
+cv2.destroyAllWindows()
